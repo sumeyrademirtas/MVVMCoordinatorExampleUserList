3e22078079e8e24f4cffa63e1f440c3492be8488
Sumeyra Demirtas <smyrdmrts@gmail.com>
2024-12-24 22:09:15 +0300

    Initial Commit

---
+++ MVVMCoordinatorExampleUserList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMCoordinatorExampleUserList
+//
+//  Created by Sümeyra Demirtaş on 12/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
